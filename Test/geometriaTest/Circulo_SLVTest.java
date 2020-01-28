a8a7485e3a047854be0ec82f1029d6c01555c3cd
pelop <pelop@RayDark>
2020-01-28 21:10:22 +0100

    Uff ahora sí

---
+++ Test/geometriaTest/Circulo_SLVTest.java
@@ -0,0 +1,24 @@
+package geometriaTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Circulo_SLVTest {
+
+	@Test
+	public void testArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPerimetro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCirculo_SLV() {
+		fail("Not yet implemented");
+	}
+
+}
