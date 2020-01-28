eb7111cb20848b82dff6685ca58b610efa20b651
pelop <pelop@RayDark>
2020-01-28 20:36:17 +0100

    versión refactorizada

--- src/Circulo_SLV.java
+++ src/Circulo_SLV.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_SLV extends FiguraGeometrica_SLV {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_SLV(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- src/FiguraGeometrica_SLV.java
+++ src/FiguraGeometrica_SLV.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_SLV {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_SLV(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
